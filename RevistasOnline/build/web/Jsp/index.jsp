70fd337874ab3dacd3f949d02efdd6447ba4e436
jhonny <jhonnygarcia201830454@cunoc.edu.gt>
2019-09-09 16:36:07 -0600

    First commit

---
+++ RevistasOnline/build/web/Jsp/index.jsp
@@ -0,0 +1,26 @@
+<%-- 
+    Document   : index
+    Created on : 7/09/2019, 09:55:08 PM
+    Author     : jhonny
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../Css/Style2.css">
+        <title>Magazines online</title>
+    </head>
+    <body>
+        <header >
+            <%@include file="../Html/menu-bar.html" %>
+        </header>     
+         <div class="contenedor">
+            
+
+
+        </div>    
+       
+    </body>
+</html>
---
+++ RevistasOnline/src/java/ejemplo/Usuario.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author jhonny
+ */
+public class Usuario {
+    private String nombre;
+    private String pass;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
